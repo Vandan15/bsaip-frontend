d5548e4770b173ab5a7fe8a50e2d95d1ffa0a242
Rinki Singh <rinkisinghdevninja@gmail.com>
2023-07-13 12:40:26 +0530

    init2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   compress: false,
   output: {
     // ...
--- pages/_app.js
+++ pages/_app.js
@@ -15,69 +15,9 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 import Aos from 'aos';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    verifyToken();
     Aos.init();
   }, [])
-  const verifyToken = () => {
-    const cookie = getCookie('token');
-    const username = getCookie('username');
-    const email = getCookie('email');
-    const cart = getCookie('cart');
-    const role = getCookie('role');
-    const user = getCookie('user');
-    console.log('cookie here',cookie)
-    if (cookie !== undefined && cookie !== "undefined") {
-      //check token API
-      setAuthorizationToken(`Bearer ${cookie}`, null, null, false);
-      if (cart !== undefined && cart !== "undefined") {
-        store.dispatch(setUserInfo(
-          {
-            cookie,
-            role,
-            username,
-            email,
-            cart: JSON.parse(cart).data,
-            user: user && JSON.parse(user),
-            is_logged: (cookie) == 'undefined' || cookie == undefined ? false : true
-          }));
-      } else {
-        store.dispatch(setUserInfo(
-          {
-            cookie,
-            username,
-            role,
-            email,
-            user: JSON.parse(user),
-            is_logged: (cookie) == 'undefined' || cookie == undefined ? false : true
-          }));
-      }
-      setLoading(false);
-    } else if (cart !== undefined && cart !== "undefined") {
-      setLoading(false);
-      if (cart !== undefined && cart !== "undefined") {
-        store.dispatch(setUserInfo(
-          {
-            cart: JSON.parse(cart).data,
-            // user: JSON.parse(user),
-          }));
-      }
-    } else {
-      setLoading(false);
-    }
-
-  }
-  const getCookie = (cName) => {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie); //to be careful
-    const cArr = cDecoded.split('; ');
-    let res;
-    cArr.forEach(val => {
-      if (val.indexOf(name) === 0) res = val.substring(name.length);
-    })
-    return res;
-  }
   return (
     <>
       <Head>
